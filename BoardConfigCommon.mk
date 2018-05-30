8940e376336d459fd0faf537fed9d97b87f43dfe
absarrahman <raman040635@gmail.com>
2018-05-30 05:09:40 +0000

    msm8956-common: Set vfat and exfat driver to sdfat
    This commit also needs sdfat support in kernel
    Signed-off-by: Absar <raman040635@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -155,8 +155,9 @@ TARGET_FS_CONFIG_GEN := $(VENDOR_PATH)/config.fs
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(VENDOR_PATH)/compatibility_matrix.xml
 
-# exFat
-TARGET_EXFAT_DRIVER := exfat
+# sdFat
+TARGET_EXFAT_DRIVER := sdfat
+TARGET_VFAT_DRIVER := sdfat
 
 # FM
 BOARD_HAVE_QCOM_FM := true
